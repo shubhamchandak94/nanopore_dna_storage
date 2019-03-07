f82eaea4702a7db94654a27e4864be31dc491757
Shubham Chandak <schandak@stanford.edu>
2019-03-07 07:13:47 -0800

    parallel LVA

--- shubham/simulator_conv_LVA.py
+++ shubham/simulator_conv_LVA.py
@@ -10,20 +10,23 @@ import distance
 import util
 PATH_TO_CPP_EXEC = "/raid/nanopore/shubham/flappie/shubham/viterbi_nanopore.out"
 PATH_TO_FLAPPIE = "/raid/nanopore/shubham/flappie/flappie"
-NUM_TRIALS = 1
+NUM_TRIALS = 10
+LIST_DECODING_MODE = 1
+LIST_SIZE = 1
+NUM_THR = 30
+
 SYN_SUB_PROB = 0.0
 SYN_DEL_PROB = 0.0
 SYN_INS_PROB = 0.0
 msg_len = 142 # after attaching sync markers
 # Note: markers not attached for the padding.
-sync_marker = ''
+sync_marker = '110'
 sync_marker_period = 9 # first marker at 0, second at sync_marker_period
 msg_len_before_sync_markers = msg_len - (msg_len//sync_marker_period)*len(sync_marker) - min([msg_len%sync_marker_period,len(sync_marker)]) # subtract marker lengths from complete periods and last incomplete period
 print('msg_len_before_sync_markers',msg_len_before_sync_markers)
 
-hamming_list = []
-edit_list = []
-correct_list = []
+correct_list_top = []
+correct_list_list = []
 
 for _ in range(NUM_TRIALS):
     msg_before_markers = np.random.choice(['0','1'], msg_len_before_sync_markers)
@@ -57,23 +60,25 @@ for _ in range(NUM_TRIALS):
     post_filename = 'tmp.'+rnd+'.post'
     decoded_filename = 'tmp.'+rnd+'.dec'
     subprocess.run([PATH_TO_FLAPPIE, fast5_filename, '--post-output-file', post_filename])
-    subprocess.run([PATH_TO_CPP_EXEC,'decode',post_filename,decoded_filename,str(msg_len),'-l','1','1','30'])
-    subprocess.run([PATH_TO_CPP_EXEC,'decode',post_filename,decoded_filename,str(msg_len)])
+    subprocess.run([PATH_TO_CPP_EXEC,'decode',post_filename,decoded_filename,str(msg_len),'-l',str(LIST_DECODING_MODE),str(LIST_SIZE),str(NUM_THR)])
     with open(decoded_filename) as f:
-        decoded_msg = f.read()
+        decoded_msg_list = [l.rstrip('\n') for l in f.readlines()]
 
     # remove temporary files
-#    os.remove(fast5_filename)
-#    os.remove(post_filename)
-#    os.remove(decoded_filename)
+    os.remove(fast5_filename)
+    os.remove(post_filename)
+    os.remove(decoded_filename)
 
-    print(decoded_msg)
-    hamming_list.append(distance.hamming(msg,decoded_msg))
-    print('Hamming distance',hamming_list[-1])
-    edit_list.append(distance.levenshtein(msg,decoded_msg))
-    print('Edit distance',edit_list[-1])
-    correct_list.append((hamming_list[-1] == 0))
+    print('Top message:')
+    print(decoded_msg_list[0])
+    print('List size:',len(decoded_msg_list))
+    top_correct = (decoded_msg_list[0] == msg)
+    list_correct = (msg in decoded_msg_list)
+    print('Top correct:', top_correct)
+    print('List correct:', list_correct)
+    correct_list_top.append(top_correct)
+    correct_list_list.append(list_correct)
 print('Summary statistics:')
 print('Number total:', NUM_TRIALS)
-print('Number correct:', sum(correct_list))
-print('Average bit error rate:', sum(hamming_list)/(msg_len_before_sync_markers*NUM_TRIALS))
+print('Number top correct:', sum(correct_list_top))
+print('Number list correct:', sum(correct_list_list))
