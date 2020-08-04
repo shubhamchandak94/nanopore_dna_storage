27fd26e2ae817c72a7ab6e3b08ddb378e2f8acaa
Shubham Chandak <schandak@stanford.edu>
2020-08-04 10:34:26 -0700

    new pool

---
+++ oligos_8_4_20/decoding_scripts/run_decoder_exp_16.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ROOT_PATH=/raid/nanopore/shubham/20200214_nanopore_pool_data/
+
+penalty=0.5
+model_folder=$ROOT_PATH/nanopore_dna_storage/bonito_model/model_train_on_oligo_1_2_4_5_8_9_10_11_on_pretrained_lr_e-6_test_on_oligo_3_global_accuracy_weights_100/
+
+HDF5_INPUT=$ROOT_PATH/data/raw_signal/raw_signal_16.hdf5
+DIRNAME=$ROOT_PATH/data/decoded_lists/exp_16/
+READ_ID_FILE=$ROOT_PATH/data/read_ids/exp_16_read_ids.19818.txt
+mkdir -p $DIRNAME
+python3 -u generate_decoded_lists.py \
+--hdf_file $HDF5_INPUT \
+--out_prefix $DIRNAME/list \
+--read_id_file $READ_ID_FILE \
+--info_file $DIRNAME/info.txt \
+--mem_conv 8 \
+--msg_len 188 \
+--rate_conv 7 \
+--list_size 32 \
+--start_barcode TGATCTATGATCGATGTACAGCGCG \
+--end_barcode TCTCACGCGCACGATCTCTGACACT \
+--num_threads 4 \
+--barcode_extend_penalty $penalty \
+--bonito_model_path $model_folder
+# comment out bonito_model_path line for default model
---
+++ oligos_8_4_20/encode_experiments.py
@@ -0,0 +1,100 @@
+import sys
+sys.path.insert(1, '../')
+import helper
+
+barcode_start = [
+        'GCTACATGTATACTGCGAGACAGAC',
+        'TCTATCTACTCGTGCTCGCTAGCTG',
+        'TGAGATCACAGCTACATAGTGAGAG',
+        'AGCGTACACGACTGAGCACACTACG',
+        'CATCAGCAGTAGAGAGTAGCGCGAT',
+        'GAGTCTCTAGCGCTACGAGATATAT',
+        'CACGAGATCTCAGTGTCGACACGTG',
+        'CGCTGCAGTCTATCTCTCTGTACAT',
+        'GTGACTCTGCATATGCTGTCTCGAT',
+        'ATGAGAGAGAGCGCTCTCTCATGAG',
+        'GTCGTACTGTGACTCTCACTCGTGC',
+        'CGCACGCATATCACGATGAGTAGCT',
+        'CGTATGCTCGTATGAGCATAGAGTG',
+        'GTGTGAGCTATGCGAGCGACGATCT',
+        'TATGTACTAGCTAGTCACGCACACA',
+        'CTAGACGTGCGAGTATACTACTATG',
+        'TGATCTATGATCGATGTACAGCGCG',
+        'TCTCGCTCGAGCACAGAGATAGCGA',
+        ]
+
+barcode_end = [
+        'CGATAGTCGCAGTCGCACATCACTC',
+        'TGTCTGCACTGCACTAGTCGCATGT',
+        'GATAGAGCACTGTCGTGTGCAGTCA',
+        'ATGCGCTCACACGCATCTGCTAGCA',
+        'TATCATCGACGCTAGCAGTGTCTGC',
+        'AGCTCTGATGAGATCAGCAGACTGT',
+        'AGTGATAGTGATCTCGACGCAGCTA',
+        'GCTAGTAGAGATACATCTCGCGAGA',
+        'CTGAGTGTGTGCACGTGAGATGCAC',
+        'ACAGCGCGATAGATGTACATAGTAC',
+        'TCTGCGTACGTGTCGACACATGTCT',
+        'TGCGAGACATATAGTAGCTCTCTCA',
+        'GTCGTCTAGATGTATCGATGTGATG',
+        'CGTCGACGTCGTACACTGTACGTCA',
+        'ACACTGCTGTGTCGTCAGTCTCACG',
+        'ATGACACGCAGTCAGACGCGTGTGA',
+        'TCTCACGCGCACGATCTCTGACACT',
+        'ACATGTATCTGTAGATGCTGTGAGC',
+         ]
+
+RS_redundancy = [ 0.25 ]*18
+
+conv_m = ([6, 8, 11]*3)*2
+
+conv_r = ([3]*3+[5]*3+[7]*3)*2
+
+pad = [
+        True,
+        False,
+        False,
+        False,
+        False,
+        False,
+        True,
+        False,
+        True,
+        ] + \
+      [
+        False,
+        True,
+        True,
+        True,
+        False,
+        True,
+        False,
+        False,
+        False,
+        ]
+
+bytes_per_oligo = ([18]*3 + [20]*3 + [22]*3) + ([16]*3+[20]*3+[20]*3)
+
+DATA_PATH='./'
+infile_name = [DATA_PATH+'data_files.tar.bz2.enc.1']*9 + [DATA_PATH+'data_files.tar.bz2.enc.2']*9
+for i in range(18):
+    print('-------------------------------------\n')
+    print('i',i)
+    print('bytes_per_oligo',bytes_per_oligo[i])
+    print('RS_redundancy',RS_redundancy[i])
+    print('conv_m',conv_m[i])
+    print('conv_r',conv_r[i])
+    print('pad',pad[i])
+
+    if (i < 9):
+        print('using encode with 1 CRC')
+        helper.encode(data_file = infile_name[i], oligo_file = DATA_PATH+'reads.'+str(i), bytes_per_oligo = bytes_per_oligo[i], RS_redundancy = RS_redundancy[i], conv_m = conv_m[i], conv_r = conv_r[i], pad=pad[i])
+    else:
+        print('using encode with 2 CRCs')
+        helper.encode_2crc(data_file = infile_name[i], oligo_file = DATA_PATH+'reads.'+str(i), bytes_per_oligo = bytes_per_oligo[i], RS_redundancy = RS_redundancy[i], conv_m = conv_m[i], conv_r = conv_r[i], pad=pad[i])
+
+    with open(DATA_PATH+'reads.'+str(i)) as f_reads, open(DATA_PATH+'oligos_'+str(i)+'.fa','w') as f_oligos:
+        for j, line in enumerate(f_reads):
+            f_oligos.write('>oligos_'+str(i)+'_'+barcode_start[i]+'_'+barcode_end[i]+'_'+str(j)+'\n')
+            f_oligos.write(barcode_start[i]+line.rstrip('\n')+barcode_end[i]+'\n')
+
